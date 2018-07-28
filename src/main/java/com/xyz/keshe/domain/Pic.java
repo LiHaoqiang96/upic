eac378cab4ffbecff884e417eeb764be50faeda9
lihaoqiang96 <lihaoqiang_yx@163.com>
2018-07-28 19:42:24 +0800

    mybatis-generator

---
+++ src/main/java/com/xyz/keshe/domain/Pic.java
@@ -0,0 +1,43 @@
+package com.xyz.keshe.domain;
+
+public class Pic {
+    private Integer pId;
+
+    private Integer mId;
+
+    private String pUrl;
+
+    private Integer pDownload;
+
+    public Integer getpId() {
+        return pId;
+    }
+
+    public void setpId(Integer pId) {
+        this.pId = pId;
+    }
+
+    public Integer getmId() {
+        return mId;
+    }
+
+    public void setmId(Integer mId) {
+        this.mId = mId;
+    }
+
+    public String getpUrl() {
+        return pUrl;
+    }
+
+    public void setpUrl(String pUrl) {
+        this.pUrl = pUrl == null ? null : pUrl.trim();
+    }
+
+    public Integer getpDownload() {
+        return pDownload;
+    }
+
+    public void setpDownload(Integer pDownload) {
+        this.pDownload = pDownload;
+    }
+}
---
+++ src/main/java/com/xyz/keshe/mapper/PicMapper.java
@@ -0,0 +1,30 @@
+package com.xyz.keshe.mapper;
+
+import com.xyz.keshe.domain.Pic;
+import com.xyz.keshe.domain.PicExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface PicMapper {
+    long countByExample(PicExample example);
+
+    int deleteByExample(PicExample example);
+
+    int deleteByPrimaryKey(Integer pId);
+
+    int insert(Pic record);
+
+    int insertSelective(Pic record);
+
+    List<Pic> selectByExample(PicExample example);
+
+    Pic selectByPrimaryKey(Integer pId);
+
+    int updateByExampleSelective(@Param("record") Pic record, @Param("example") PicExample example);
+
+    int updateByExample(@Param("record") Pic record, @Param("example") PicExample example);
+
+    int updateByPrimaryKeySelective(Pic record);
+
+    int updateByPrimaryKey(Pic record);
+}
